7761b1e431f375277ba4d08021543a1dc6305b02
William Griffin <williamgriffin989@gmail.com>
2015-01-07 17:41:29 -0500

    undef. error

--- day3.rb
+++ day3.rb
@@ -1,13 +1,15 @@
-puts "Human, see if you can guess my number between 1 and a 100. What is your number?"
-gets.chomp = g 
-answer = 5 
-if g == answer
+i = 0
+answer = rand(101) + 1
+while i < 5 do 
+puts ("Human, see if you can guess my number between 1 and a 100. What is your number?")
+i += 1 
+gets.chomp 
+num = gets.chomp  
+if num == answer
 puts "WIN"
-elsif g > answer 
+elsif num > answer
 puts "Guess lower Human"
-elsif g < answer
+elsif num < answer
 puts "Guess higher Human"  
 end 
-
-#### 
-#test
+end
